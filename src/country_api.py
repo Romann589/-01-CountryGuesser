0c7131130e7f68012175ffb8192e2408aef5bd73
Romann Kalbreyer <romann@MacBook-Pro-von-Romann.local>
2026-06-10 09:14:42 +0200

    reset

--- src/country_api.py
+++
@@ -1,9 +0,0 @@
-import json
-import requests
-
-url = "https://restcountries.com/v3.1/all?fields=cca3,flags,borders,capital,continents,translations,languages,population"
-response = requests.get(url)
-if response.status_code == 200:
-    countries = response.json()
-with open("countries.json", "w", encoding="utf-8") as file:
-    json.dump(countries, file, indent=4, ensure_ascii=False)
